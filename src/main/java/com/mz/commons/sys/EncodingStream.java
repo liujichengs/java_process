82011638e142650e3aea8bd806d2389ab7655615
unknown <ljc124854@hz.ali.com>
2016-12-06 19:34:58 +0800

    修改时间

--- src/main/java/com/mz/commons/sys/EncodingStream.java
+++ src/main/java/com/mz/commons/sys/EncodingStream.java
@@ -1,40 +1,38 @@
 package com.mz.commons.sys;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 
+/**
+ * 2016-12-06
+ */
 public class EncodingStream extends Thread {
-	private InputStream is;
-	private OutputStream os;
+    private InputStream is;
+    private OutputStream os;
 
-	public EncodingStream(InputStream is, OutputStream redirect) {
-		this.is = is;
-		this.os = redirect;
-	}
+    public EncodingStream(InputStream is, OutputStream redirect) {
+        this.is = is;
+        this.os = redirect;
+    }
 
-	public void run() {
-		try {
-			PrintWriter pw = null;
-			if (os != null){
-				pw = new PrintWriter(os);
-			}
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				if (pw != null) {
-					pw.println(line);
-				}
-				 System.out.println(">>" + line);
-			}
-			if (pw != null)
-				pw.flush();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
+    public void run() {
+        try {
+            PrintWriter pw = null;
+            if (os != null) {
+                pw = new PrintWriter(os);
+            }
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                if (pw != null) {
+                    pw.println(line);
+                }
+                System.out.println(">>" + line);
+            }
+            if (pw != null)
+                pw.flush();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 }
