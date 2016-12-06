77657426140af99482782814fdb3a8bba6f55fda
unknown <ljc124854@hz.ali.com>
2016-12-06 20:18:40 +0800

    第二次修改分支

--- src/main/java/com/mz/commons/sys/EncodingStream.java
+++ src/main/java/com/mz/commons/sys/EncodingStream.java
@@ -3,8 +3,9 @@ package com.mz.commons.sys;
 import java.io.*;
 
 /**
- * 2016-12-06
+ * 2016-12-06对对对
  * 第一次修改
+ * 分支第二次修改
  */
 public class EncodingStream extends Thread {
     private InputStream is;
