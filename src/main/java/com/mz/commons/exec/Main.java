0456fc64b723e283f2fae64137bd408e1b2755b4
liujichengs@gmail.com <ljc521521>
2016-12-07 17:18:35 +0800

    01

--- src/main/java/com/mz/commons/exec/Main.java
+++ src/main/java/com/mz/commons/exec/Main.java
@@ -2,6 +2,9 @@ package com.mz.commons.exec;
 
 public class Main {
 
+    /**
+     * 第01次修改
+     */
 	static void synchronously() {
 		Spawn.synExec(
 				new String[] { "/bin/bash", "-c", "vmstat -s -S M | egrep -ie 'memory|swap' | grep 'free memory'" },
